c81ddd45f3e9a3695f0eb0755622fe523b0b7ac7
Tim3720 <timkals@gmail.com>
2022-08-24 11:59:26 +0200

    added median segmenter

--- segmenter_mutliprocessing.py
+++ segmenter_mutliprocessing.py
@@ -28,8 +28,8 @@ class SegmenterMultiProcessing:
     def __init__(self, img_path, save_path):
         self.img_path = img_path
         self.save_path = save_path
-        self.filenames = os.listdir(img_path) * 2
-        self.filenames = self.filenames[:1000]
+        self.filenames = os.listdir(img_path)
+        self.filenames = self.filenames
         self.filenames = list(map(lambda x: os.path.join(img_path, x), self.filenames))
         self.n_files = len(self.filenames)
 
@@ -40,7 +40,8 @@ class SegmenterMultiProcessing:
         manager = Manager()
         self.bg_mean_vals = manager.list([0 for _ in range(self.n_files)])
         self.img_mean_vals = manager.list([0 for _ in range(self.n_files)])
-        self.counter = manager.list([0])
+        self.counter = manager.list([0, 0])
+        self.next_img_queue_median = manager.Queue(5)
 
         self.min_area = 400
 
@@ -48,7 +49,7 @@ class SegmenterMultiProcessing:
         s = 0
         for fn in fns:
             img = cv.imread(fn, cv.IMREAD_GRAYSCALE).astype(np.float64)
-            img = cv.resize(img, (2000, 2000))
+            # img = cv.resize(img, (2560, 2560))
             s += img  # type: ignore
         sum_list.append(s)
 
@@ -75,7 +76,7 @@ class SegmenterMultiProcessing:
         i = start_index
         while i <= end_index:
             img = cv.imread(self.filenames[i], cv.IMREAD_GRAYSCALE)
-            img = cv.resize(img, (2000, 2000))
+            # img = cv.resize(img, (2560, 2560))
             queue.put(img)
             i += 1
 
@@ -84,14 +85,14 @@ class SegmenterMultiProcessing:
         while i <= end_index:
             fn = self.filenames[i]
             img = cv.imread(fn)
-            img = cv.resize(img, (2000, 2000))
+            # img = cv.resize(img, (2560, 2560))
             queue.put((cv.cvtColor(img, cv.COLOR_BGR2GRAY), img, fn.split("\\")[-1]))
             i += 1
 
     def detect(self, corrected, img, fn):
         thresh = cv.threshold(corrected, 0, 255, cv.THRESH_TRIANGLE)[1]
         cnts = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
-
+        counter = 0
         for cnt in cnts:
             if cv.contourArea(cnt) < self.min_area:
                 continue
@@ -101,7 +102,12 @@ class SegmenterMultiProcessing:
             var = np.var(crop_corr)
             if var < 30:
                 continue
-            cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            counter += 1
+            cv.imwrite(
+                os.path.join(self.save_path, fn[:-4] + f"_{counter}.jpg" + fn), crop
+            )
+            # cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
             # cv.putText(
             #     img,
             #     str(var),
@@ -112,13 +118,15 @@ class SegmenterMultiProcessing:
             #     1,
             # )
 
-        cv.imwrite(
-            os.path.join(self.save_path, fn),
-            img,
-        )
+        # cv.imwrite(
+        #     os.path.join(self.save_path, fn),
+        #     img,
+        # )
 
         self.counter[0] += 1
-        if (self.counter[0] / self.n_files * 100) % 10 == 0:
+        p = (self.counter[0] / self.n_files * 100) // 10 * 10
+        if p > self.counter[1]:
+            self.counter[1] = p  # type: ignore
             print(f"{(self.counter[0] * 100 // self.n_files)}% done...")
 
     def mean_segmenter(self, indices):
@@ -175,7 +183,9 @@ class SegmenterMultiProcessing:
                 print("Waiting...", threading.enumerate())
                 time.sleep(0.01)
 
-            t = Thread(target=self.detect, args=(corrected, current_img, f"{i}.jpg"))
+            t = Thread(
+                target=self.detect, args=(corrected, current_img, fn.split("\\")[-1])
+            )
             t.start()
             threads.append(t)
 
@@ -218,13 +228,81 @@ class SegmenterMultiProcessing:
         # plt.close()
         return total_duration
 
+    def preload_median(self, fns):
+        for i in range(len(fns)):
+            imgs = []
+            if i <= self.median_bg_size // 2:
+                start_index = 0
+                img_index = i
+                end_index = self.median_bg_size
+            elif i >= len(fns) - self.median_bg_size // 2:
+                start_index = len(fns) - self.median_bg_size
+                img_index = i - len(fns)
+                end_index = len(fns) - 1
+            else:
+                start_index = i - self.median_bg_size // 2
+                end_index = i + self.median_bg_size // 2 + 1
+                img_index = self.median_bg_size // 2 + 1
+
+            filenames = []
+            for j in range(start_index, end_index):
+                img = cv.imread(fns[j], cv.IMREAD_GRAYSCALE)
+                img = cv.resize(img, (2560, 2560))
+                imgs.append(img)
+                filenames.append(fns[j])
+            fn = filenames[img_index]
+            self.next_img_queue_median.put((imgs, fn))
+
+    def median_segmenter(self, index):
+        next_imgs, fn = self.next_img_queue_median.get()
+        img = cv.imread(fn)
+        bg = np.median(next_imgs, axis=0).astype(np.uint8)
+        bg = cv.resize(bg, (5120, 5120))
+        corrected = cv.absdiff(cv.cvtColor(img, cv.COLOR_BGR2GRAY), bg)
+        self.detect(corrected, img, fn.split("\\")[-1])
+
+    def main_median_segmenter(self, cores):
+        self.cores = cores
+
+        start = time.perf_counter()
+
+        splitted = split(self.filenames, 5)
+
+        for fns in splitted:
+            p = Process(target=self.preload_median, args=(fns,))
+            p.start()
+
+        print(f"Files: {self.n_files}; Cores: {self.cores}")
+        with Pool(self.cores) as pool:
+            pool.map(self.median_segmenter, range(self.n_files))
+
+        end = time.perf_counter()
+        total_duration = end - start
+
+        print(
+            f"Total time: {total_duration: .2f}s",
+            f"Avg time per img: {total_duration / len(self.filenames): .2f}s",
+        )
+
+        # plt.plot(self.img_mean_vals, "bo", label="Mean values of images")
+        # plt.plot(
+        #     self.bg_mean_vals,
+        #     label="Mean values of background images",
+        #     color="orange",
+        # )
+        # plt.legend()
+        # plt.savefig(os.path.join(save_path, "development_of_mean_values_median.png"))
+        # plt.close()
+        return total_duration
+
 
 if __name__ == "__main__":
     img_path = "C:/Users/timka/Documents/Arbeit/Testprofil-M181-CTD-035-JPG"
-    save_path = "Results/Test_multiprocessing_Mean"
+    save_path = "Results/Crops_Mean_Half_Resolution"
 
     for fn in os.listdir(save_path):
         os.remove(os.path.join(save_path, fn))
 
     s = SegmenterMultiProcessing(img_path, save_path)
-    duration = s.main_mean_segmenter(3, 5)
+    duration = s.main_mean_segmenter(5, 6)
+    # duration = s.main_median_segmenter(11)
